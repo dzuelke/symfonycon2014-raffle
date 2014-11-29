e13dc3123c340da77dd13f138a2224ca9968a058
David Zuelke <dzuelke@gmail.com>
2014-11-29 15:02:55 +0100

    dynamically resolve to drawing specific templates if available

--- src/Dzuelke/SymfonyCon2014/Bundle/RaffleBundle/Controller/DefaultController.php
+++ src/Dzuelke/SymfonyCon2014/Bundle/RaffleBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -32,7 +33,8 @@ class DefaultController extends Controller
         
         shuffle($entrants);
         
-        return array('winner' => $entrants[0], 'raffle' => $raffle);
+        $winner = $entrants[0];
+        return new Response($this->get('twig')->resolveTemplate(array("DzuelkeSymfonyCon2014RaffleBundle:Default:draw.$raffle.html.twig", 'DzuelkeSymfonyCon2014RaffleBundle:Default:draw.html.twig'))->render(array('winner' => $winner, 'raffle' => $raffle)));
     }
     
     /**
